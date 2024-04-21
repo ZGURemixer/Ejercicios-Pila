cd50d05868fb181a3603dc5afff5811ef0905c62
ZGURemixer <zonagamerultra@outlook.com>
2024-04-21 01:06:04 -0400

    Ran through a code beautifier.

--- ej5.c
+++ ej5.c
@@ -1,32 +1,33 @@
 #include "stack.h"
 #define VERBOSE_MODE FALSE
 
-float get_sum(Stack stack)
+float
+get_sum(Stack stack)
 {
-	Element sum = 0;
-	
-	do{
-		sum += peek(stack, FALSE);
-		pop(&stack);	
-	}while(stack.top > EMPTY);
-	
-	return sum;
+    Element sum = 0;
+
+    do {
+        sum += peek(stack, FALSE);
+        pop(&stack);
+    } while (stack.top > EMPTY);
+
+    return sum;
 }
 
-static Stack stack = {0};
+static Stack stack = { 0 };
 
-int main(void)
+int
+main(void)
 {
-	int i=0;
-	float average=0;
-	initialize(&stack, assign_max());
-	
-	for(i=0; i<=stack.max; i++)
-	{
-		push(&stack, NULL_ELEMENT, TRUE, VERBOSE_MODE);
-	}
-	
-	print_stack(stack, VERBOSE_MODE);
-	printf("Average: %.1f", average = get_sum(stack) / (float)(stack.top+1));
-	return SUCCESS;
+    int i = 0;
+    float average = 0;
+    initialize(&stack, assign_max());
+
+    for (i = 0; i <= stack.max; i++) {
+        push(&stack, NULL_ELEMENT, TRUE, VERBOSE_MODE);
+    }
+
+    print_stack(stack, VERBOSE_MODE);
+    printf("Average: %.1f", average = get_sum(stack) / (float)(stack.top + 1));
+    return SUCCESS;
 }
--- stack.h
+++ stack.h
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define TRUE 1
 #define FALSE 0
 #define SUCCESS 0
@@ -13,169 +13,167 @@
 typedef short unsigned int Bool;
 typedef int Element;
 
-typedef struct Stack {
-	Element array[MAX_STACK];
-	int top;
-	int max;
+typedef struct Stack
+{
+    Element array[MAX_STACK];
+    int top;
+    int max;
 } Stack;
 
-Element assign_element(int id)
+Element
+assign_element(int id)
 {
-	Element element = NULL_ELEMENT;
-	
-	do{
-		printf("Insert element %d: ", id+1);
-		fflush(stdin);
-	}while(!scanf("%d", &element) || element == NULL_ELEMENT);
-	
-	return element;
+    Element element = NULL_ELEMENT;
+
+    do {
+        printf("Insert element %d: ", id + 1);
+        fflush(stdin);
+    } while (!scanf("%d", &element) || element == NULL_ELEMENT);
+
+    return element;
 }
 
-void initialize(Stack *stack, int max)
+void
+initialize(Stack* stack, int max)
 {
-	int i;
-	
-	if(max <= MIN_STACK || max > MAX_STACK)
-	{
-		fprintf(stderr, "Error: Invalid value assigned to stack.\n");
-		exit(FAILURE);
-	} else {
-		stack->max = --max;
-		stack->top = EMPTY;
-		
-		for(i=0; i<=stack->max; i++)
-		{
-			stack->array[i] = NULL_ELEMENT;
-		}
-	}
+    int i;
+
+    if (max <= MIN_STACK || max > MAX_STACK) {
+        fprintf(stderr, "Error: Invalid value assigned to stack.\n");
+        exit(FAILURE);
+    } else {
+        stack->max = --max;
+        stack->top = EMPTY;
+
+        for (i = 0; i <= stack->max; i++) {
+            stack->array[i] = NULL_ELEMENT;
+        }
+    }
 }
 
-void push(Stack *stack, Element element, Bool user, Bool verbose)
+Bool
+is_empty(Stack stack)
 {
-	if(stack->top == stack->max)
-	{
-		fprintf(stderr, "Error: Overflow\n");
-		exit(FAILURE);
-	} else {
-		if(element != NULL_ELEMENT)
-		{
-			if(verbose)
-			{
-				printf("Element [%d] should be pushed.\n", element);
-			}
-			stack->array[++stack->top] = element;
-		} else {
-			if(user)
-			{
-				++stack->top;
-				stack->array[stack->top] = assign_element(stack->top);
-			} else {
-				fprintf(stderr, "Error: Invalid value assigned to stack.\n");
-				exit(FAILURE);				
-			}
-		}
-	}
+    return stack.top <= EMPTY;
 }
 
-void pop(Stack *stack)
+Bool
+is_full(Stack stack)
 {
-	if(stack->top == EMPTY)
-	{
-		fprintf(stderr, "Error: Underflow\n");
-		exit(FAILURE);
-	} else {
-		stack->array[stack->top--] = NULL_ELEMENT;
-	}
+    return stack.top == stack.max;
 }
 
-Bool is_empty(Stack stack)
+void
+push(Stack* stack, Element element, Bool user, Bool verbose)
 {
-	return stack.top <= -1;
+    if (is_full(*stack)) {
+        fprintf(stderr, "Error: Overflow\n");
+        exit(FAILURE);
+    } else {
+        if (element != NULL_ELEMENT) {
+            if (verbose) {
+                printf("Element [%d] should be pushed.\n", element);
+            }
+            stack->array[++stack->top] = element;
+        } else {
+            if (user) {
+                ++stack->top;
+                stack->array[stack->top] = assign_element(stack->top);
+            } else {
+                fprintf(stderr, "Error: Invalid value assigned to stack.\n");
+                exit(FAILURE);
+            }
+        }
+    }
 }
 
-Bool is_full(Stack stack)
+void
+pop(Stack* stack)
 {
-	return stack.top == stack.max;
+    if (is_empty(*stack)) {
+        fprintf(stderr, "Error: Underflow\n");
+        exit(FAILURE);
+    } else {
+        stack->array[stack->top--] = NULL_ELEMENT;
+    }
 }
 
-Element peek(Stack stack, Bool verbose)
+Element
+peek(Stack stack, Bool verbose)
 {
-	if(verbose){
-		printf("Top element should be [%d].\n", stack.array[stack.top]);
-	}
-	return stack.array[stack.top];
+    if (verbose) {
+        printf("Top element should be [%d].\n", stack.array[stack.top]);
+    }
+    return stack.array[stack.top];
 }
 
-void print_stack(Stack stack, Bool verbose)
+void
+print_stack(Stack stack, Bool verbose)
 {
-	int i;
-	
-	for(i=0; i<=stack.max; i++)
-	{
-		if(stack.array[i] != NULL_ELEMENT)
-		{
-			printf("[%d]", stack.array[i]);
-		} else {
-			printf("[ ]");
-		}
-	}
-	
-	printf("\n");
-	
-	if(verbose)
-	{
-		printf("Max: %d\n", stack.max);
-		printf("Top: %d\n", stack.top);
-	}
+    int i;
+
+    for (i = 0; i <= stack.max; i++) {
+        if (stack.array[i] != NULL_ELEMENT) {
+            printf("[%d]", stack.array[i]);
+        } else {
+            printf("[ ]");
+        }
+    }
+
+    printf("\n");
+
+    if (verbose) {
+        printf("Max: %d\n", stack.max);
+        printf("Top: %d\n", stack.top);
+    }
 }
 
-int assign_max(void)
+int
+assign_max(void)
 {
-	int max;
-	
-	do {
-		printf("Insert stack size: ");
-		fflush(stdin);
-	} while (!scanf("%d", &max) || max<=MIN_STACK || max>MAX_STACK);
-	
-	return max;
+    int max;
+
+    do {
+        printf("Insert stack size: ");
+        fflush(stdin);
+    } while (!scanf("%d", &max) || max <= MIN_STACK || max > MAX_STACK);
+
+    return max;
 }
 
-int find_first_non_null(Stack stack, Bool verbose)
+int
+find_first_non_null(Stack stack, Bool verbose)
 {
-	int i;
-	
-	for(i=0; i<=stack.top; i++)
-	{
-		if(verbose)
-		{
-			printf("Checked: [%d]\n", stack.array[i]);
-		}
-		if(stack.array[i]!=NULL_ELEMENT)
-		{
-			return i;
-		}
-	}
-	
-	fprintf(stderr, "Error: Invalid stack.\n");
-	exit(FAILURE);
-	return FAILURE;
+    int i;
+
+    for (i = 0; i <= stack.top; i++) {
+        if (verbose) {
+            printf("Checked: [%d]\n", stack.array[i]);
+        }
+        if (stack.array[i] != NULL_ELEMENT) {
+            return i;
+        }
+    }
+
+    fprintf(stderr, "Error: Invalid stack.\n");
+    exit(FAILURE);
+    return FAILURE;
 }
 
-void reverse_stack(Stack *stack, Bool verbose)
+void
+reverse_stack(Stack* stack, Bool verbose)
 {
-	static Stack backup = {0};
-	backup = *stack;
-	
-	initialize(stack, backup.max+1);
-	
-	do{
-		push(stack, peek(backup, verbose), FALSE, verbose);
-		pop(&backup);		
-	}while(backup.top >= FIRST_INDEX);
-	
-	if(verbose)
-	{
-		printf("Stack has been reversed successfully.\n");
-	}
+    static Stack backup = { 0 };
+    backup = *stack;
+
+    initialize(stack, backup.max + 1);
+
+    do {
+        push(stack, peek(backup, verbose), FALSE, verbose);
+        pop(&backup);
+    } while (backup.top >= FIRST_INDEX);
+
+    if (verbose) {
+        printf("Stack has been reversed successfully.\n");
+    }
 }
